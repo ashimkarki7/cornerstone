4fc43d06a17d95d0c0628c5c93fdef5ddf2a9c4e
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2025-10-17 22:16:47 +0545

    Added error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@/styles/global.scss';
+import {ErrorBoundary} from '@/hoc/ErrorBoundary.tsx';
 
 
 
@@ -10,7 +11,9 @@ import '@/styles/global.scss';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-          <App />
+        <ErrorBoundary>
+            <App />
+        </ErrorBoundary>
     </Router>
   </StrictMode>
 );
